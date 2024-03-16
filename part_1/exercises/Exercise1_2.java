a3cc996a17c48c17ecc44fd003d0a46d0ea4d618
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-16 20:35:51 +0100

    task 2

---
+++ part_1/exercises/Exercise1_2.java
@@ -0,0 +1,9 @@
+package part_1.exercises;
+
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        String name = "Alana";
+        String position = "Garden Manager";
+        System.out.println("my name is " + name + " and I want to be a " + position + " " + " in Mesozoic Eden.");
+    }
+}
