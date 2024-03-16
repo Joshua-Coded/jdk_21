8be3dc5cb6017b34ffc54511c32508d417c23a9e
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-16 21:57:42 +0100

    task 5

---
+++ part_1/exercises/Exercise1_5.java
@@ -0,0 +1,8 @@
+package part_1.exercises;
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        String name = "Joshua Alana";
+        System.out.println("Welcome to " + name + " " + " to Mesozoic Eden!.");
+    }
+}
