14cfc3c3322c4a9ca8cd9b59adcfdcd0adacf4c2
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-16 22:08:05 +0100

    project for week 1

---
+++ part_1/project/Main.java
@@ -0,0 +1,12 @@
+package part_1.project;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alana";
+        String openingHour = "08:00";
+        String closingHour = "20:00";
+        System.out.println("Welcome " + name + "  " + " to Mesozoic Eden!");
+        System.out.println("We open at " + openingHour + " " + "and close at " + closingHour);
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}
