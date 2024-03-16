6be81d680f271a92b9ca2d91915671fd16cb7667
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-16 22:02:41 +0100

    task 6

---
+++ part_1/exercises/Exercise1_6.java
@@ -0,0 +1,9 @@
+package part_1.exercises;
+
+public class Exercise1_6 {
+    public static void main(String[] args) {
+        String name = "Joshua Alana";
+        System.out.println("Welcome " + name + " " + "to Mesozoic Eden!");
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}
