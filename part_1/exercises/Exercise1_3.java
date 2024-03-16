48304a48e2ab5bc1b86085dadfd0782ad0d4cedd
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-16 21:42:10 +0100

    task 3

---
+++ part_1/exercises/Exercise1_3.java
@@ -0,0 +1,9 @@
+package part_1.exercises;
+
+public class Exercise1_3 {
+    public static void main(String[] args) {
+        String openingHour = "08:00";
+        String closingHour = "20:00";
+        System.out.println("We open at " + openingHour + " and close at " + closingHour + ".");
+    }
+}
