72018c2ffa80b366227f14e1b3afd7eb2c966442
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:15:35 +0100

    task 7 for exercise 2

---
+++ part_2/exercises/Exercise2_7.java
@@ -0,0 +1,13 @@
+package part_2.exercises;
+
+public class Exercise2_7 {
+    public static void main(String[] args) {
+        // declare the variables
+        String dinoName = "Rezynski";
+        int dinoAge = 10;
+        String diet = "Carnivore";
+
+        // print them out
+        System.out.println("Dinosaur Name: " + dinoName + " Age: " + dinoAge + " years, Diet: " + diet);
+    }
+}
