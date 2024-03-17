a9f0dee78716027f65824a4d422951c911e28ae3
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:02:33 +0100

    task 4 for exercise 2

---
+++ part_2/exercises/Exercise2_4.java
@@ -0,0 +1,13 @@
+package part_2.exercises;
+
+public class Exercise2_4 {
+    public static void main(String[] args) {
+        // declare variables
+        String employeeName = "Joshua Alana";
+        int employeeAge = 17;
+
+        // print the variables
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee Age: " + employeeAge + " years");
+    }
+}
