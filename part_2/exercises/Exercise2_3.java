f48b73098eb5bb0e27c68fc544a26e81f692babb
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 14:59:26 +0100

    task 3 for exercise 2

---
+++ part_2/exercises/Exercise2_3.java
@@ -0,0 +1,11 @@
+package part_2.exercises;
+
+public class Exercise2_3 {
+    public static void main(String[] args) {
+        // declare variables
+        int maxVisitors = 12000; // per day
+
+        // print variables
+        System.out.println("There's a maximum of " + maxVisitors + " people allowed to Mesozoic Eden per Day.");
+    }
+}
