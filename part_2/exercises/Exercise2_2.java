ee6844c25fda290de4b4214a3eee1fa847e19ca8
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 14:54:53 +0100

    task 2 for exercise 2

---
+++ part_2/exercises/Exercise2_2.java
@@ -0,0 +1,15 @@
+package part_2.exercises;
+
+public class Exercise2_2 {
+    public static void main(String[] args) {
+        // declaring variables
+        int age = 10;
+        String name = "Rexy";
+        boolean isCarnivore = true;
+
+        // printing variables
+        System.out.println("Age: " + age + "years old");
+        System.out.println("Name: " + name);
+        System.out.println("IsCarnivore: " + (isCarnivore ? "YES" : "NO"));
+    }
+}
