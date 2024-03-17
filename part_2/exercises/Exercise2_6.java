e7fa4a15cef77448818170b5924f73a2eedf08c8
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:10:40 +0100

    task 6 for exercise 2

---
+++ part_2/exercises/Exercise2_6.java
@@ -0,0 +1,11 @@
+package part_2.exercises;
+
+public class Exercise2_6 {
+    public static void main(String[] args) {
+        // declare varibles
+        int safetyRating = 8;
+
+        // print out the rating
+        System.out.println("Park's Safety Rating: " + safetyRating + "/10");
+    }
+}
