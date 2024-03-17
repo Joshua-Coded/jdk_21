76e1d8413f2180bc7c980a8174042f197971a0fa
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:26:55 +0100

    project completed

---
+++ part_2/project/Main.java
@@ -0,0 +1,14 @@
+package part_2.project;
+
+public class Main {
+    public static void main(String[] args) {
+        String dinoName = "Rexyth";
+        int dinoAge = 10;
+        String species = "T-Rex";
+        String diet = "Carnivore";
+        double weight = 9000;
+
+        System.out.println("Meet " + dinoName + ", a " + dinoAge + "-year-old " + species + ". As a " + diet
+                + ", it has a robust weight of " + weight + " kilograms.");
+    }
+}
