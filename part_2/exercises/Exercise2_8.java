cc74c2efd1e5b8aec6fd14dab218d3b9e85d2fc3
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:18:53 +0100

    last task of exercise

---
+++ part_2/exercises/Exercise2_8.java
@@ -0,0 +1,9 @@
+package part_2.exercises;
+
+public class Exercise2_8 {
+    public static void main(String[] args) {
+        char specialFirstName = 'T';
+
+        System.out.println("First leter of dinosaur species: " + specialFirstName);
+    }
+}
