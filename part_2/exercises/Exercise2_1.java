17940e51ba84ea42b4f588d5fb8024056c43038b
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 14:50:10 +0100

    task 1 for exercise 2

---
+++ part_2/exercises/Exercise2_1.java
@@ -0,0 +1,18 @@
+package part_2.exercises;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+
+        // declaring variables
+        String breed = "T-REX";
+        double height = 13.5;
+        double lenght = 21.2;
+        double weight = 90000;
+
+        // printing the variables
+        System.out.println("Breed: " + breed);
+        System.out.println("Height: " + height + "m");
+        System.out.println("Weight: " + weight + "kg");
+        System.out.println("Length: " + lenght + "m");
+    }
+}
