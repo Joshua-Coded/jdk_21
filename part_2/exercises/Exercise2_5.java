913ad3058fbfb3cce7993ba2befa29212d0de0c1
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-17 15:06:17 +0100

    task 5 for exercise 2

---
+++ part_2/exercises/Exercise2_5.java
@@ -0,0 +1,11 @@
+package part_2.exercises;
+
+public class Exercise2_5 {
+    public static void main(String[] args) {
+        // declare variables
+        int numberOfDinosaurs = 60;
+
+        // print them out
+        System.out.println("Number of Dinosuars:  " + numberOfDinosaurs);
+    }
+}
