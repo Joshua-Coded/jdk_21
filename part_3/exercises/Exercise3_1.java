593f412de60c2f356ba20f03ee6d5cee1457d737
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-19 11:01:14 +0100

    exercise 1

---
+++ part_3/exercises/Exercise3_1.java
@@ -0,0 +1,11 @@
+package part_3.exercises;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        double dino1Weight = 8000;
+        double dino2Weight = 5000;
+
+        double averageWeight = (dino1Weight + dino2Weight) / 2;
+        System.out.println("Average Weight: " + averageWeight + "kg");
+    }
+}
