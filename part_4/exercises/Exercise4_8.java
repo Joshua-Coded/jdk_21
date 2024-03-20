884d2a6045f46f0d2f650400b601dfe4e62f6337
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-20 18:32:08 +0100

    week 4 task 8

---
+++ part_4/exercises/Exercise4_8.java
@@ -0,0 +1,13 @@
+package part_4.exercises;
+
+public class Exercise4_8 {
+    public static void main(String[] args) {
+        int time = 9;
+
+        if (time >= 10 && time <= 19) {
+            System.out.println("The park is open for day visitors!");
+        } else {
+            System.out.println("The park is closed for day visitors!");
+        }
+    }
+}
