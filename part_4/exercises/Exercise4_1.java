71f22b782fe888612606f6b4fbfdb9ffbe7cd151
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-20 17:26:08 +0100

    week 4 task 1

---
+++ part_4/exercises/Exercise4_1.java
@@ -0,0 +1,13 @@
+package part_4.exercises;
+
+public class Exercise4_1 {
+    public static void main(String[] args) {
+        boolean isCanivore = true;
+
+        if (isCanivore) {
+            System.out.println("The dinosaur is a carnivore");
+        } else {
+            System.out.println("The dinosaur is a herbivore");
+        }
+    }
+}
