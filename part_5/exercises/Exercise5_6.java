1fa7f98eb7267b9a3307764b710067f0e321bf89
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-21 14:59:31 +0100

    week 5 task 6

---
+++ part_5/exercises/Exercise5_6.java
@@ -0,0 +1,12 @@
+package part_5.exercises;
+
+public class Exercise5_6 {
+    public static void main(String[] args) {
+        boolean allChecks = false;
+
+        do {
+            System.out.println("Performing security check...");
+            allChecks = true;
+        } while (!allChecks);
+    }
+}
