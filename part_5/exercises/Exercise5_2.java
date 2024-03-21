cd942aed42d58d67facd8dd9c548b64bfc8bff5a
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-21 14:22:14 +0100

    week 5 task 2

---
+++ part_5/exercises/Exercise5_2.java
@@ -0,0 +1,11 @@
+package part_5.exercises;
+
+public class Exercise5_2 {
+    public static void main(String[] args) {
+        boolean isHungry = true;
+        do {
+            System.out.println("Feeding the dinosaur...");
+            isHungry = false;
+        } while (isHungry);
+    }
+}
