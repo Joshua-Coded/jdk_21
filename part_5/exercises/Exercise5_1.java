e3283c8b8086e4f0adbd365a5815606520282049
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-21 14:02:26 +0100

    week 5 task 1

---
+++ part_5/exercises/Exercise5_1.java
@@ -0,0 +1,9 @@
+package part_5.exercises;
+
+public class Exercise5_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println("dino: " + i);
+        }
+    }
+}
