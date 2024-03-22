f88a21dabc5fc4d53271547f1807d83f004ea65a
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 15:52:41 +0100

    test

--- part_6/Test.java
+++ part_6/Test.java
@@ -13,14 +13,19 @@ public class Test {
         // System.out.println(results[i]);
         // }
 
-        double[] results = { 7.0, 8.0, 9.0, 5.0, 3.0, 4.0 };
-        double total = 0;
-        for (int i = 0; i < results.length; i++) {
-            total += results[i];
+        // double[] results = { 7.0, 8.0, 9.0, 5.0, 3.0, 4.0 };
+        // double total = 0;
+        // for (int i = 0; i < results.length; i++) {
+        // total += results[i];
+        // }
+
+        // double average = total / results.length;
+        // System.out.println(average);
+
+        int[] nums = { 23, 45, 67, 98, 454, 235, 232, 454 };
+
+        for (int num : nums) {
+            System.out.println(num);
         }
-
-        double average = total / results.length;
-        System.out.println(average);
-
     }
 }
