c3eb717587c16bfdf83bac01f9cba9d0bd9baa71
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 16:14:38 +0100

    four dimensional array

--- part_6/Test.java
+++ part_6/Test.java
@@ -22,10 +22,41 @@ public class Test {
         // double average = total / results.length;
         // System.out.println(average);
 
-        int[] nums = { 23, 45, 67, 98, 454, 235, 232, 454 };
+        // int[] nums = { 23, 45, 67, 98, 454, 235, 232, 454 };
 
-        for (int num : nums) {
-            System.out.println(num);
+        // for (int num : nums) {
+        // System.out.println(num);
+        // }
+
+        // int[][] matrixs = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        // for (int i = 0; i < matrix.length; i++) {
+        // for (int j = 0; j < matrix[i].length; j++) {
+        // System.out.println(matrix[i][j] + "");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int[] row : matrixs) {
+        // for (int element : row) {
+        // System.out.println(element + " ");
+        // }
+        // System.out.println();
+        // }
+
+        int[][][][] matrixs = { { { { 1, 2, 3, 4 } } }, { { { 5, 6, 7, 8 } } }, { { { 9, 10, 11, 12 } } },
+                { { { 13, 14, 15, 16 } } } };
+
+        for (int[][][] row : matrixs) {
+            for (int[][] elements : row) {
+                for (int[] el : elements) {
+                    for (int e : el) {
+                        System.out.println(e);
+                    }
+                    System.err.println();
+                }
+                System.out.println();
+            }
+            System.out.println();
         }
     }
 }
