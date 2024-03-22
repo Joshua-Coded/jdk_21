eac147c4082ddd55a3a1eaeaf61059343362dc2a
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 15:48:10 +0100

    test

---
+++ part_6/Test.java
@@ -0,0 +1,26 @@
+package part_6;
+
+public class Test {
+    public static void main(String[] args) {
+        // int[] a = { 1, 2, 3, 4, 5, 6 };
+        // int b = a[0];
+        // System.out.println(b);
+        // System.out.println(a);
+
+        // access arrays using for loops and for each iteration
+        // int[] results = { 10, 49, 23, 40, 50 };
+        // for (int i = 0; i < results.length; i++) {
+        // System.out.println(results[i]);
+        // }
+
+        double[] results = { 7.0, 8.0, 9.0, 5.0, 3.0, 4.0 };
+        double total = 0;
+        for (int i = 0; i < results.length; i++) {
+            total += results[i];
+        }
+
+        double average = total / results.length;
+        System.out.println(average);
+
+    }
+}
