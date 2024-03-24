ea5b640dbc2e2beb88239188f5e45c95770703ef
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-24 18:10:08 +0100

    task 6

---
+++ part_6/exercises/Exercise6_6.java
@@ -0,0 +1,8 @@
+package part_6.exercises;
+
+public class Exercise6_6 {
+    public static void main(String[] args) {
+        String[][] parkMap = { { "Enclosure 1", " Facility1" }, { "Enclosure 2", " Facility2" },
+                { "Enclosure 3", " Facility 3" } };
+    }
+}
