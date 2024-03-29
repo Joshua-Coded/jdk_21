6bbb7fc27e789e79fd29defdbd0b60eec90424a1
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-29 22:48:34 +0100

    task 5 week 7

---
+++ part_7/exercises/Exercise7_5.java
@@ -0,0 +1,8 @@
+package part_7.exercises;
+
+public class Exercise7_5 {
+
+    public String getGreeting(String dinosaurName, String visitorName) {
+        return "WELCOME  " + visitorName + "! yOUR FAVAOURITE DINOSAUR " + dinosaurName + " IS EXCITED TO SEE YOU!";
+    }
+}
