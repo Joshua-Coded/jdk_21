b5ebaeb99b68e798c7419c273e9a9533fea7e49b
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-29 21:04:26 +0100

    task 3 week 7

---
+++ part_7/exercises/Exercise7_3.java
@@ -0,0 +1,11 @@
+package part_7.exercises;
+
+public class Exercise7_3 {
+    public double getAverageAge(int[] ages) {
+        int totalAge = 0;
+        for (int age : ages) {
+            totalAge += age;
+        }
+        return (double) totalAge / ages.length;
+    }
+}
