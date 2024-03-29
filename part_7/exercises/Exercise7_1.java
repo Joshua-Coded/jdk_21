efcaabc410837f1060531401d2bd24eb8bbf8d90
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-29 20:15:10 +0100

    task 1 week 7

---
+++ part_7/exercises/Exercise7_1.java
@@ -0,0 +1,13 @@
+package part_7.exercises;
+
+public class Exercise7_1 {
+    public String getLifeStage(int age) {
+        if (age < 5) {
+            return "Hatchling";
+        } else if (age < 15) {
+            return "Juvenile";
+        } else {
+            return "Adult";
+        }
+    }
+}
